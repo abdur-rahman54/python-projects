20d2349803e2853d62d5030cff03a9b89c61f31e
abdur-rahman54 <abdur.rahman59354@gmail.com>
2022-12-12 17:57:05 -0500

    First code for leap year checking

---
+++ leap_year_checker.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+def is_leap_year(year):
+	if year % 4 == 0:
+		if year % 100 == 0:
+			if year % 400 == 0:
+				print('{} Year is a leap year'.format(year))
+			else:
+				print('{} Year is not a leap year'.format(year))	
+		else:
+			print('{} Year is a leap year'.format(year))
+	else:
+		print('{} Year is not a leap year'.format(year))
+		
+is_leap_year(2000)
