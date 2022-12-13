58f552971ea9224f67d9509b802c3e1a8daf2daf
abdur-rahman54 <abdur.rahman59354@gmail.com>
2022-12-13 12:07:03 -0500

    version 1.0

--- binary_search.py
+++ binary_search.py
@@ -11,3 +11,32 @@ def naive_search(l, target):
 		if l[i] == target:
 			return i
 	return -1
+	
+#binary serach uses devide and conquer
+#we will leverage the fact that our list is sorted
+
+def binary_search (l, target, low = None, high = None):
+	if low is None:
+		low = 0
+	if high is None:
+		high = len(l) - 1
+	
+	if high < low:
+		return -1
+
+
+	midpoint = (low +  high) // 2
+	
+	if l [midpoint] == target:
+		return midpoint
+	elif target < l[midpoint]:
+		return binary_search (l, target, low, midpoint-1)
+	else:
+		#target > l [midpoint]
+		return binary_search (l, target, midpoint+1, high)
+	
+if __name__ == '__main__':
+	l = [1, 3, 5, 7, 10, 14]
+	target = 10
+	print (naive_search (l, target))
+	print (binary_search (l, target))
