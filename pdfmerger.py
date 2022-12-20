177e84d605470d1f8a68425e23c3aa9763dbade0
abdur-rahman54 <abdur.rahman59354@gmail.com>
2022-12-20 08:59:29 -0500

    Version 1.0

--- pdfmerger.py
+++ pdfmerger.py
@@ -1,8 +1,18 @@
 #!/bin/python
 
 
-import PyPDF
+import PyPDF2
 import sys
+import os
+
+merger = PyPDF2.PdfFileMerger()
+
+for file in os.listdir(os.curdir):
+	if file.endswith(".pdf"):
+		
+		merger.append(file)
+	merger.write("combined_PDF.pdf")
+		
 
 
 
